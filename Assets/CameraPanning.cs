fef27addc37bac12c1d6dd14d67bc98d10646f21
Amm0n1te <103222407+Amm0n1te@users.noreply.github.com>
2022-10-16 21:48:09 -0700

    made 4 cameras, camera panning operational

--- Assets/CameraPanning.cs
+++ Assets/CameraPanning.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class CameraPanning : MonoBehaviour
 {
+
+    public Camera pancam;
+    public Camera firstcam;
+    public Camera thirdcam;  //default main cam
+    public Camera orthocam;
+    private float increment;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+ 
+        increment = 0.05f;
+        thirdcam.enabled = false;
+        orthocam.enabled = false;
+        pancam.enabled = true;
+        firstcam.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //GetComponent<Camera>().transform.Rotate(0f, 0f, increment, Space.Self);
+        pancam.transform.Rotate(0f, increment, 0f, Space.Self);
+        if (pancam.transform.localEulerAngles.y > 359) increment *= -1;
+        else if (pancam.transform.localEulerAngles.y < 250) increment *= -1;
+        Debug.Log(pancam.transform.localEulerAngles.y.ToString() + "  and " + increment.ToString());
         
     }
 }
